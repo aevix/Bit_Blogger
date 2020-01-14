f37181022fa4f5f0b319bee7be1e9cd27de6876a
aevix <55609333+aevix@users.noreply.github.com>
2020-01-13 23:17:37 -0600

    finish creating functions
    created functions for creating connection, creating connection, making tables, add, delete, edit, pull query, and insert from data base.

--- item_data_base.py
+++ item_data_base.py
@@ -2,7 +2,11 @@ import sqlite3
 from sqlite3 import Error
 
 def create_connection(db_file):
-
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
     conn=None
     try:
         conn = sqlite3.connect(db_file)
@@ -11,27 +15,76 @@ def create_connection(db_file):
         print(e)
     
     return conn
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-
-def create_table(conn, statement):
+def insert_data(conn, item):
     """
     Create a new project into the projects table
     :param conn:
     :param project:
     :return: project id
     """
-    sql = ''' INSERT INTO projects(item_id, type , size, yn)
-              VALUES(?,?,?,?) '''
-    c = conn.cursor()
-    c.execute(sql, statement)
-    return c.lastrowid
+    sql = ''' INSERT INTO projects(id, type, size, outgoing) VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, item)
+    return cur.lastrowid
+
+def update_item(conn, new_data):
+
+    sql = ''' UPDATE projects
+              SET outgoing = ?
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, new_data)
+    conn.commit()
+
+def delete_item(conn, id):
+    sql = 'DELETE from projects WHERE id = ?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    conn.commit()
+
+def delete_all(conn):
+    sql = 'Delete from projects'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
+def select_all(conn):
+    cur = conn.cursor()
+    cur.execute('select * from projects')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def select_by_id(conn,id):
+    sql = 'select * from projects where id =?'
+    cur = conn.cursor()
+    cur.execute(sql, (id,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 def main():
-    database = r"pythonsqlite.db"
+    database = r"C:\Users\April\Documents\GitHub\item_tracker\pythonsqlite.db"
     conn = create_connection(database)
+
     with conn:
-        item = (4548156548, 'suit', 'large', 'y')
-        item_id = create_table(conn,item)
+        print("Query printed by id")
+        select_by_id(conn, 4546456)
+        print("Query printed all")
+        select_all(conn)
+
 
 if __name__ == '__main__':
     main()
