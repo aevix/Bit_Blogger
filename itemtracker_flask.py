32ee795e558db36d278db4416837468ac67e90a9
aevix <55609333+aevix@users.noreply.github.com>
2020-01-19 15:01:08 -0600

    update flask file

--- itemtracker_flask.py
+++ itemtracker_flask.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import item_data_base
 
 app=Flask(__name__)
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI']='pythonsqlite.db'
+
+
+@app.route('/', methods =['POST','GET'])
 def index():
+    if request.method == 'POST':
+        select_id = request.form['barcode']
+        new_id = select_by_id(conn, barcode=select_id)
+    else:
+        pass
     return render_template("item_UI.html")
 if __name__=="__main__":
     app.run(debug=True)
