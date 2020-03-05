e3bf897785e485a7e2b8943dc6ea4e838b54a1ba
aevix <55609333+aevix@users.noreply.github.com>
2020-03-05 10:21:44 -0600

    add page count

--- app/__init__.py
+++ app/__init__.py
@@ -33,8 +33,7 @@ if not app.debug:
             app.logger.addHandler(mail_handler)
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/itemtracker.log', maxBytes=10240,
-                                       backupCount=10)
+    file_handler = RotatingFileHandler('logs/itemtracker.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
--- app/routes.py
+++ app/routes.py
@@ -71,11 +71,12 @@ def user(username):
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    print(posts)
     next_url = url_for('user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url, current_page=posts.page, pages=posts.pages)
 
 @app.before_request
 def before_request():
